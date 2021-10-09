f1de324e4ada3f22aa28ef952cc21c5e01f2053a
Vlad Sorokin <homiecrewtm@gmail.com>
2021-10-09 15:17:12 +0600

    add link to to github CV link app

--- src/Layout/BaseLayout/index.jsx
+++ src/Layout/BaseLayout/index.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
+import GithubRibbon from '../../components/GithubRibbon';
 import './index.css';
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <GithubRibbon />
+      <div className="layout">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/GithubRibbon/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+export default function GithubRibbon() {
+  return (
+    <a href="https://github.com/parabolabam/CV" className="github-ribbon">
+      <img
+        loading="lazy"
+        width="149"
+        height="149"
+        src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+        className="attachment-full size-full"
+        alt="Fork me on GitHub"
+        data-recalc-dims="1"
+      />
+
+    </a>
+  );
+}
