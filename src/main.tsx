9aafe31e299b7e5fe81cb1e65b99a5d2f890d889
parabolabam <sorokinvladislav1402@gmail.com>
2023-10-22 19:16:10 +0400

    update base href

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 // hydrate for SSR
 import ReactDOM from 'react-dom';
-
-import './css/global-classes.css';
-import './css/fonts.css';
-import './css/index.css';
+import React from 'react';
+import App from './App';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  fab, faInstagram, faTwitter, faGithub,
-} from '@fortawesome/free-brands-svg-icons';
-import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons';
+import { faGithub, faInstagram, faTwitter, fab } from '@fortawesome/free-brands-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
-import App from './App';
-import React from 'react';
+import './css/fonts.css';
+import './css/global-classes.css';
+import './css/index.css';
 
 library.add(fab, faInstagram, faTwitter, faGithub, faSun, faMoon, faExternalLinkAlt);
 
